d5308d0ff85601ee52ce8e51a589db516b5675f6
Omar <omar.branez@gmail.com>
2021-08-19 00:48:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions here
+function map(source, callbackFunction) {
+    const newArray = [] // not "let"
+    for (const el of source) {
+        newArray.push(callbackFunction(el))
+    }
+    return newArray
+}
+
+function reduce(source, callbackFunction, startingPoint) {
+    let total // it's an integer, not an array nor an object
+    if (startingPoint) {
+        total = startingPoint
+        for (const el of source) {
+            total = callbackFunction(el, total)
+        }
+        return total
+    } else {
+        total = source[0]
+        source.shift() // get rid of the first element, it's been used
+        for (const el of source) {
+            total = callbackFunction(el, total)
+        }
+        return total
+    }
+}
